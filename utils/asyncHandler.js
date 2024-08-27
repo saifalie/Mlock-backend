754b5947aba240dc9f8b9fa488037624059514f6
saif ali shaikh <saifalie14@gmail.com>
2024-08-27 20:45:07 +0530

    add asyncHandler

---
+++ utils/asyncHandler.js
@@ -0,0 +1,11 @@
+
+
+export const asyncHandler = (requestHandler) => {
+    return (req, res, next) => {
+        Promise
+            .resolve(requestHandler(req, res, next))
+            .catch((error) => next(error))
+
+    }
+}
+
