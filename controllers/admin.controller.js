21a93467c4e4fb13246730415166ed50c85f9d5f
saif ali shaikh <saifalie14@gmail.com>
2024-08-27 21:20:36 +0530

    add create lockerStation controller

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,4 +1,5 @@
 import LockerLocation from "../models/lockerLocation.model.js";
+import LockerStation from "../models/lockerStation.model.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
@@ -33,7 +34,27 @@ export const createLockerLocation = asyncHandler(async (req, res) => {
 })
 
 
-export const createLockerStation = asyncHandler(async(req,res)=>{
+export const createLockerStation = asyncHandler(async (req, res) => {
 
-    const {location}
+    const { location, name } = req.body
+
+    try {
+        const lockerStation = await LockerStation.create({
+            location,
+            name
+        })
+
+        console.log("Successfully created the locker Station");
+
+        return res.status(200).json(new ApiResponse(200, lockerStation, "Successfully created the locker Station"))
+
+
+
+    } catch (error) {
+        console.log("Failed to create the lockerStation ", error);
+        throw new ApiError(500, "Failed to create the locker Station", error)
+
+    }
 })
+
+
--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { createLockerLocation } from "../controllers/admin.controller.js";
+import { createLockerLocation, createLockerStation } from "../controllers/admin.controller.js";
 
 
 const adminSchema = Router()
 
 adminSchema.route("/lockerLocation").post(createLockerLocation)
+adminSchema.route("/lockerStation").post(createLockerStation)
+
 
 export default adminSchema
